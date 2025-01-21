3e6096a9908cf3996ff4542582081ea4a8d48ed9
Gerhardus Coenraad Potgieter <25999656@sun.ac.za>
2025-01-21 09:36:38 +0200

    Just spent like the last hour constructing a tree purely to use as examples, will now attempt the problem :)

---
+++ medium/236-lowest-common-ancestor-of-a-binary-tree/src/main.cpp
@@ -0,0 +1,210 @@
+
+
+
+
+#include <stdexcept>
+#include <vector>
+#include <iostream>
+#include <optional>
+#include <queue>
+
+/* * Definition for a binary tree node. */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+
+class Solution {
+private:
+
+    void binarySearch(TreeNode* root, int target) {
+
+    }
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+
+    }
+};
+
+
+void printTree(TreeNode* root) {
+
+    // print bfs
+    std::queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+
+        TreeNode* currNode = q.front();
+        q.pop();
+
+        if (!currNode) {
+            std::cout << "null, ";
+            continue;
+        } else {
+            std::cout << currNode->val << ", ";
+        }
+
+        q.push(currNode->left);
+        q.push(currNode->right);
+    }
+
+}
+
+TreeNode* createTree(const std::vector<std::optional<int>>& inpArr) {
+
+
+
+    int dataVal;
+    TreeNode* root = nullptr;
+    int numNulls = 0;
+    int nullsSeen = 0;
+
+    for (int i = 0; i < inpArr.size(); i++) {
+
+        if (!inpArr[i].has_value()) {
+            std::cout << "NULL VALUE -> Do Nothing..." << std::endl;
+            numNulls += 1;
+            continue;
+        }
+
+        dataVal = *inpArr[i];
+        std::cout << "Creating Node #" << i << " With Data Value: " << dataVal << std::endl;
+
+        TreeNode* newNode = new TreeNode(dataVal);
+
+        if (!root) {
+            root = newNode;
+        } else {
+
+            nullsSeen = 0;
+            // bfs to insert at nullptr
+            std::queue<TreeNode*> q;
+            q.push(root);
+            while (!q.empty()) {
+                TreeNode* currNode = q.front();
+                q.pop();
+
+                if (currNode->left) {
+                    q.push(currNode->left);
+                } else {
+                    if (nullsSeen >= numNulls) {
+                        currNode->left = newNode;
+                        break;
+                    }
+                    nullsSeen += 1;
+                }
+                if (currNode->right) {
+                    q.push(currNode->right);
+                } else {
+                    if (nullsSeen >= numNulls) {
+                        currNode->right = newNode;
+                        break;
+                    }
+                    nullsSeen += 1;
+                }
+            }
+        }
+
+    }
+    return root;
+}
+
+TreeNode* deleteTree(TreeNode* root, bool silent) {
+
+
+
+    if (!root) return nullptr;
+
+    root->left = deleteTree(root->left, silent);
+    root->right = deleteTree(root->right, silent);
+
+    if (!silent) {
+        std::cout << "Deleting Node With Value: " << root->val << std::endl;
+    }
+    delete root;
+
+    return nullptr;
+}
+
+TreeNode* findNode(TreeNode* root, int element) {
+
+    std::queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+
+        TreeNode* currNode = q.front();
+        q.pop();
+
+        if (currNode->val == element) {
+            return currNode;
+        }
+
+        if (currNode->left) q.push(currNode->left);
+        if (currNode->right) q.push(currNode->right);
+    }
+
+    std::string outpError = "Could not find node with value: " + std::to_string(element);
+    throw std::invalid_argument(outpError);
+}
+
+
+
+
+int main() {
+
+    std::vector<std::optional<int>> tree1 = { 1,2,3,4,5,6, std::nullopt};
+    std::vector<std::optional<int>> tree2 = {3,5,1,6,2,0,8,std::nullopt,std::nullopt,7,4};
+    int p1 = 5;
+    int q1 = 8;
+
+    std::vector<std::optional<int>> tree = tree2;
+
+    std::cout << "\nCreating Tree..." << std::endl;
+    TreeNode* root = createTree(tree);
+
+    std::cout << "\nPrinting Tree..." << std::endl;
+    printTree(root);
+
+
+    TreeNode* p = findNode(root, 5);
+    TreeNode* q = findNode(root, 1);
+
+
+
+    Solution sol;
+
+
+
+
+    std::cout << "\n\n";
+    std::cout << "Input:\nTree: ";
+    for (const auto& elem : tree) {
+        if (elem.has_value()) {
+            std::cout << *elem << ", ";
+        } else {
+            std::cout << "null, ";
+
+        }
+    }
+    std::cout << std::endl;
+    std::cout << "p: " << p->val << std::endl;
+    std::cout << "q: " << q->val << std::endl;
+
+    std::cout << "\nOutput: " << sol.lowestCommonAncestor(root, p, q) << std::endl;
+
+    std::cout << "\n\nDeleting Tree..." << std::endl;
+    deleteTree(root, true);
+    return 0;
+
+
+
+
+
+
+
+}
